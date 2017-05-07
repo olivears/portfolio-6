2fada766b654d95b8c603d64609c3b9fd62db06e
Florent Landuré <f.landure@gmail.com>
2017-05-07 15:07:06 +0200

    links updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SocialMediaComponent } from './social-media/social-media.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
